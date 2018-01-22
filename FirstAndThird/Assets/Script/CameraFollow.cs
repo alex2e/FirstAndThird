7e52a3780316613cd26796806c1caf2aaff9d6d6
DESKTOP-7U6IS1N\Alejandro <a.santillana@aol.com>
2018-01-22 09:08:18 +0100

    Ejemplo de primera y tercera persona

---
+++ FirstAndThird/Assets/Script/CameraFollow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    //Campo que representa al objeto Jugador.
+    public GameObject player;
+    //Variable para almacenar la distancia entre la cámara y el jugador.
+    private Vector3 offset;
+
+    // Use this for initialization
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void LateUpdate()
+    {
+        // Se establece la posición de la cámara para que sea la misma que
+        // más la distancia calculada al principio.
+        transform.position = player.transform.position + offset;
+    }
+}
